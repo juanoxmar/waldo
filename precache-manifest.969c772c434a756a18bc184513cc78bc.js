b5a56b162603b1712971b10fcc0ff53bb670a1b8
juanoxmar <joxmar.ramirez@gmail.com>
2020-08-26 14:08:05 -0700

    Updates

---
+++ precache-manifest.969c772c434a756a18bc184513cc78bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73b5c8aff3d224b7a40250762043c9a",
+    "url": "/waldo/index.html"
+  },
+  {
+    "revision": "c47fa9249b8a173d0f87",
+    "url": "/waldo/static/css/main.6319d198.chunk.css"
+  },
+  {
+    "revision": "af73dd7168f35f5c16c7",
+    "url": "/waldo/static/js/2.503369ea.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/waldo/static/js/2.503369ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47fa9249b8a173d0f87",
+    "url": "/waldo/static/js/main.82773664.chunk.js"
+  },
+  {
+    "revision": "2749c421917ef3420223",
+    "url": "/waldo/static/js/runtime-main.fbce9503.js"
+  },
+  {
+    "revision": "b975679b4f06f0eb43d8be99a4c5eaf9",
+    "url": "/waldo/static/media/odlaw.b975679b.png"
+  },
+  {
+    "revision": "2376a6973181a81987afcfd7130df02d",
+    "url": "/waldo/static/media/waldo.2376a697.jpg"
+  },
+  {
+    "revision": "e00ea340dbbc01d39ef653402cf9cc81",
+    "url": "/waldo/static/media/wally.e00ea340.png"
+  },
+  {
+    "revision": "2e6f184737d048f1fad87a7f81e5add4",
+    "url": "/waldo/static/media/wizard.2e6f1847.png"
+  }
+]);
